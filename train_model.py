f5700f67be559041940aab1e5d59e75aeb5da8e2
DanilKabanov <danilkabanov2006@bk.ru>
2026-04-25 14:53:54 +0500

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, PowerTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import mlflow
+from mlflow.models import infer_signature
+import joblib
+
+def scale_frame(frame):
+    df = frame.copy()
+    X = df.drop(columns=['charges'])
+    y = df['charges']
+    scaler = StandardScaler()
+    power_trans = PowerTransformer()
+    X_scaled = scaler.fit_transform(X)
+    y_scaled = power_trans.fit_transform(y.values.reshape(-1, 1))
+    return X_scaled, y_scaled, power_trans
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    df = pd.read_csv("./df_clear.csv")
+    X, Y, power_trans = scale_frame(df)
+    X_train, X_val, y_train, y_val = train_test_split(X, Y,
+                                                      test_size=0.3,
+                                                      random_state=42)
+    
+    params = {
+        'alpha': [0.0001, 0.001, 0.01, 0.05, 0.1],
+        'l1_ratio': [0.001, 0.05, 0.1, 0.2],
+        'penalty': ['l1', 'l2', 'elasticnet'],
+        'loss': ['squared_error', 'huber', 'epsilon_insensitive'],
+        'fit_intercept': [False, True]
+    }
+    
+    mlflow.set_experiment("insurance_charges_model")
+    with mlflow.start_run():
+        lr = SGDRegressor(random_state=42)
+        clf = GridSearchCV(lr, params, cv=3, n_jobs=4)
+        clf.fit(X_train, y_train.ravel())
+        best = clf.best_estimator_
+        y_pred = best.predict(X_val)
+        y_price_pred = power_trans.inverse_transform(y_pred.reshape(-1, 1))
+        y_val_orig = power_trans.inverse_transform(y_val)
+        rmse, mae, r2 = eval_metrics(y_val_orig, y_price_pred)
+        
+        mlflow.log_param("alpha", best.alpha)
+        mlflow.log_param("l1_ratio", best.l1_ratio)
+        mlflow.log_param("penalty", best.penalty)
+        mlflow.log_param("loss", best.loss)
+        mlflow.log_param("fit_intercept", best.fit_intercept)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+        
+        predictions = best.predict(X_train)
+        signature = infer_signature(X_train, predictions)
+        mlflow.sklearn.log_model(best, "model", signature=signature)
+        
+        with open("insurance_model.pkl", "wb") as f:
+            joblib.dump(best, f)
+    
+    df_runs = mlflow.search_runs()
+    best_run = df_runs.sort_values("metrics.r2", ascending=False).iloc[0]
+    artifact_uri = best_run['artifact_uri'].replace("file://", "") + '/model'
